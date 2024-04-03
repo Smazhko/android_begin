5e05e02f9b6a18319d2dc26d5d3f7a5c7df174bb
Peter Smazhko <smazhko@gmail.com>
2024-04-03 16:14:00 +0300

    sem03: ConstraintLayout MusicPlayer-design

---
+++ m03_constraint/app/src/main/java/ru/gb/m03_constraint/MainActivity.kt
@@ -0,0 +1,30 @@
+package ru.gb.m03_constraint
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.gb.m03_constraint.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.txtSinger.text = "ATB"
+        binding.txtSong.text = "Feel Alive (Airplay Mix)"
+
+
+
+    }
+}
