0d2688199c24035cceb6dfc1e2380a6ba7d55ea8
Peter Smazhko <smazhko@gmail.com>
2024-04-29 21:34:25 +0300

    sem10: lifecycles timer

--- m10_timer_life_cycle/app/src/main/java/ru/gb/m10_timer_life_cycle/MainActivity.kt
+++ m10_timer_life_cycle/app/src/main/java/ru/gb/m10_timer_life_cycle/MainActivity.kt
@@ -55,6 +55,11 @@ class MainActivity : AppCompatActivity() {
         slider = binding.slider
         startButton = binding.btnStart
 
+        // восстанавливаем сохраненные в Bundle значения переменных по ключам.
+        // ATOMIC типы в Bundle размещаться не могут.
+        // сразу проверяем - если поток со временем был запущен, то запускаем его снова,
+        // так как предыдущий был уже остановлен (может остановиться автоматически при закрытии
+        // или обновлении активити)
         savedInstanceState?.let { bundle ->
             Log.d("MAIN", "ПЕРЕВОРОТ **********************************")
             fullTime = bundle.getInt("fullTime")
@@ -64,11 +69,16 @@ class MainActivity : AppCompatActivity() {
             Log.d("ПЕРЕВОРОТ", "remainTime: $remainTime | fullTime: $fullTime | $isTimerRunning")
         }
 
+        // обновляем состояние всех компонентов, состояние которых важно
+        // при повторной перерисовке активити
+        // тоже самое делаем при СТАРТЕ отсчёта
         slider.value = fullTime.toFloat()
         counterTextView.text = remainTime.toString()
         progressBar.max = fullTime
         progressBar.progress = remainTime
 
+        // слушатель для слайдера - выбора времени для отсчёта
+        // обновляем переменные и обновляем состояние прогресс-бара и его текста
         slider.addOnChangeListener { _, value, _ ->
             fullTime = value.toInt()
             remainTime = value.toInt()
@@ -77,21 +87,22 @@ class MainActivity : AppCompatActivity() {
             progressBar.progress = fullTime
         }
 
-
+        // единственная кнопка, которая меняет свой текст в зависимости от состояния
         startButton.setOnClickListener {
+            // если ЕЩЕ НЕ запущен
             if (!isTimerRunning) {
                 Log.d("123", ">>>>>>>>>нажата кнопка СТАРТ")
                 startTimer()
-                // Делаем кнопку недоступной после нажатия
+                // Делаем кнопку недоступной после нажатия, потому что если часто щёлкать -
+                // глючит анимация прогресс-бара, и
+                // запускаем корутину для задержки выполнения кода на 900 миллисекунд
                 startButton.isEnabled = false
-
-                // Запускаем корутину для задержки выполнения кода на 500 миллисекунд
                 scope.launch {
-                    delay(1000)
+                    delay(900)
                     // После задержки делаем кнопку доступной
                     startButton.isEnabled = true
                 }
-            } else {
+            } else { // если УЖЕ запущен
                 Log.d("123", ">>>>>>>>>> нажата кнопка СТОП")
                 stopTimer()
             }
@@ -100,6 +111,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // сохраняем состояние активити. чтобы передать его во вновь отрисованную.
+    // сначала заполняем outState, а потом вызываем super
+    // на всякий пожарный останавливаем поток, чтоб он не работал в фоне
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putInt("fullTime", fullTime)
         outState.putInt("remainTime", remainTime)
@@ -110,12 +124,19 @@ class MainActivity : AppCompatActivity() {
         Log.d("ДО ПЕРЕВОРОТА", "СОХРАНЕНИЕ: remainTime: $remainTime | fullTime: $fullTime | $isTimerRunning")
     }
 
+    // при исчезновении активити с экрана (перерисовка или после кнопки НАЗАД останавливаем
+    // уже ненужные потоки
     override fun onPause() {
         super.onPause()
         countdownThread?.interrupt()
         countdownThread = null
     }
 
+    // обновляем переменные, состояния ВСЕХ(!!!!) элементов по этим главным переменным.
+    // если что-то не обновить, откуда ни возьми наёдутся переменные по-умолчанию  и всё испортят.
+    // создаём и запускаем ПОТОК отсчёта времени.
+    // передавать в поток данные смысла нет, так как по необходимости мы их оттуда
+    // не заберём, поэтому пусть поток пользуется глобальными переменными класса
     private fun startTimer() {
         isTimerRunning = true
 
@@ -134,6 +155,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun stopTimer() {
+        // прерываем поток
         countdownThread?.interrupt()
         countdownThread = null
 
@@ -142,7 +164,7 @@ class MainActivity : AppCompatActivity() {
         fullTime = slider.value.toInt()
         remainTime = fullTime
 
-        // сбрасываем элементы в начальное состояние
+        // сбрасываем ВСЕ!!! элементы в начальное состояние
         startButton.textSize = 26F
         startButton.text = getString(R.string.start)
         slider.isEnabled = true
@@ -153,13 +175,18 @@ class MainActivity : AppCompatActivity() {
         Log.d("stopThread", "remainTime: $remainTime | fullTime: $fullTime")
     }
 
+    // внутренний класс ПОТОК отсчёта времени
     private inner class CountdownThread() : Thread() {
         override fun run() {
-            var tag: String = Thread.currentThread().name
-            val maxSeconds = fullTime
-            val countdownHandler = Handler(Looper.getMainLooper())
+            var tag: String = Thread.currentThread().name // переменная для ЛОГа
+            val maxSeconds = fullTime // создаём копии переменных для внутреннего пользования
             var secondsRemaining = remainTime
 
+            // Handler нужен для изменения параметров жлементов НЕ из Активити, а из потока.
+            val countdownHandler = Handler(Looper.getMainLooper())
+
+            // пока есть куда считать и пока не нажали кнопку СТОП - перерисовываем элементы
+            // и отнимаем счётчик
             while (secondsRemaining >= 0 && isTimerRunning) {
                 countdownHandler.post {
                     counterTextView.text = secondsRemaining.toString()
@@ -179,24 +206,10 @@ class MainActivity : AppCompatActivity() {
                 Log.d(tag, "secondsRemaining: $secondsRemaining  | remainTime: $remainTime | maxSeconds: $maxSeconds")
             }
 
-            // Сброс состояния после завершения таймера
+            // (!!!!) ==> Сброс состояния после завершения таймера
             countdownHandler.post {
                 stopTimer()
             }
         }
     }
-
 }
-
-/*
-
-                val handler = Handler(Looper.getMainLooper())
-                thread{
-                    handler.post{
-                        for (i in )
-                        sleep(1_000)
-
-                    }
-
-                }
- */
